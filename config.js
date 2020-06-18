f500d7437e4d39420a8e737782163b0241d0be71
ridleyad <67100728+ridleyad@users.noreply.github.com>
2020-06-18 12:04:37 +0100

    Update config
    more changes

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  siteTitle: "Sarah's Business", // Site title.
+  siteTitle: 'Sarahs Business', // Site title.
   siteTitleAlt: 'Business', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://sarah-ridley-business.netlify.app/', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: "Sarah's business website", // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Sarah R business website', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
